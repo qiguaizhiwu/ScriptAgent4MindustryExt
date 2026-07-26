1b97ef800e05b0b5d97d0d83c86d52632775d3ed
Strange things <3288163665@qq.com>
2026-07-26 17:18:31 +0800

    Add text input functionality for players

---
+++ scripts/coreMindustry/utilTextInput.kts
@@ -0,0 +1,34 @@
+package coreMindustry
+
+import mindustry.game.EventType.TextInputEvent
+import cf.wayzer.scriptAgent.Event
+import cf.wayzer.scriptAgent.define.Script
+import coreLibrary.lib.util.nextEvent
+import kotlinx.coroutines.withTimeoutOrNull
+import mindustry.gen.Call
+import mindustry.gen.Player
+import kotlin.random.Random
+
+listen<TextInputEvent> {
+    OnTextInputResult(it.player, it.textInputId, it.text).launchEmit(coroutineContext + Dispatchers.game)
+}
+
+data class OnTextInputResult(val player: Player, val id: Int, val text: String?) : Event {
+    companion object : Event.Handler()
+}
+
+@Suppress("unused")
+context(script: Script)
+suspend fun textInput(
+    player: Player,
+    title: String,
+    message: String = "",
+    default: String = "",
+    lengthLimit: Int = Int.MAX_VALUE,
+    isNumeric: Boolean = false,
+    timeoutMillis: Int = 60_000
+): String? = withTimeoutOrNull(timeoutMillis.toLong()) {
+    val id = Random.nextInt(Int.MIN_VALUE, 0)
+    Call.textInput(player.con, id, title, message, lengthLimit, default, isNumeric)
+    script.nextEvent<OnTextInputResult> { it.player == player && it.id == id }.text
+}
