8350072edacce56e1e8385b9aff6d40966d9e9d4
Strange things <3288163665@qq.com>
2026-07-26 17:45:35 +0800

    Update input prompt and validation for spawnMob

--- scripts/wayzer/cmds/spawnMob.kts
+++ scripts/wayzer/cmds/spawnMob.kts
@@ -180,8 +180,8 @@ command("spawns", "打开新召唤单位菜单") {
             val selUnit = cache.unit
             val selTeam = cache.team
             if (selUnit != null && selTeam != null) {
-                val input = getInput("输入消息", "[yellow]请输入召唤数量(1-100)".with())
-                if (input = toIntOrNull()?.coerceIn(1, 100)) {
+                val input = getInput("输入消息", "[yellow]请输入召唤数量(谨慎填入)".with())
+                if (input != null) {
                     spawnUnits(p, selUnit, selTeam, input)
                 } else {
                     p.sendMessage("[red]无效数字，召唤取消".with())
