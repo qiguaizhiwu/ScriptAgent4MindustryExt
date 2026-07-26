4a54f04ab7fc93a8032fb7377864a8777f5574dd
Strange things <3288163665@qq.com>
2026-07-26 13:07:05 +0800

    Clean up imports in main.kts
    Removed unused imports from main.kts file.

--- scripts/ST/main.kts
+++ scripts/ST/main.kts
@@ -1,24 +1,8 @@
 package ST.main
 
-import cf.wayzer.placehold.PlaceHoldApi.with
-import mindustry.net.Administration
-import arc.Events
-import arc.util.Time
-import mindustry.game.EventType.GameOverEvent
 import java.time.Instant
 import kotlin.math.ceil
 import kotlin.random.Random
-import mindustry.ctype.ContentType
-import mindustry.io.SaveVersion
-import mindustry.world.Block
-import arc.struct.ObjectMap
-import arc.util.pooling.Pools
-import mindustry.game.EventType.ResetEvent
-import mindustry.world.blocks.storage.StorageBlock
-import mindustry.world.blocks.storage.StorageBlock.StorageBuild
-import mindustry.net.Administration.PlayerInfo
-import arc.util.CommandHandler
-import mindustry.gen.*
 
 command("ST", "ST插件运行状态") {
     body {
