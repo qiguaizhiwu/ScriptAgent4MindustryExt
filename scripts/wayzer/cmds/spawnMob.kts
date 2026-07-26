acd2c137bb525fde090359c08785a6d8e242f6bd
Strange things <3288163665@qq.com>
2026-07-26 17:37:33 +0800

    Update spawnMob.kts

--- scripts/wayzer/cmds/spawnMob.kts
+++ scripts/wayzer/cmds/spawnMob.kts
@@ -183,7 +183,7 @@ command("spawns", "打开新召唤单位菜单") {
                 val input = getInput("输入消息", "[yellow]请输入召唤数量(1-100)".with())
                 val input = input.toIntOrNull()?.coerceIn(1, 100)
                 if (input != null) {
-                    spawnUnits(p, selUnit, selTeam, num)
+                    spawnUnits(p, selUnit, selTeam, input)
                 } else {
                     p.sendMessage("[red]无效数字，召唤取消".with())
                 }
